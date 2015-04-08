edc8400955e4e0e84554742e0897a99734eed8b0
Raul Ramirez <raul287986@gmail.com>
2015-04-07 20:50:57 -0700

    finished video I6

--- js/entities/SmallMap.js
+++ js/entities/SmallMap.js
@@ -1,5 +1,17 @@
 game.SmallMap = me.Entity.extend({
-    
+    init: function(x, y, settings){
+        this._super(me.Entity, "init", [x, y, {
+                image: "smallmap",
+                width: 507,
+                height: 76,
+                spritewidth: "507",
+                spriteheight: "76",
+                getShape: function(){
+                    return(new meRect(0, 0, 507, 76)).toPolygon();
+                }
+        }]);
+    this.floating = true;
+    }
 });
 
 
